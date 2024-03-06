02c9959e6cdc8eabec194cc6e034c96110496e6a
BenjaminDestefanis <benjaminisraelb@gmail.com>
2024-03-06 18:09:07 -0300

    3/6/24 6:08 PM

---
+++ documents/info.py
@@ -0,0 +1,27 @@
+"""
+
+-Python
+-Django  . Recourses
+
+Python Crash Course
+
+Instalacion de Python -
+Variables y Tipos de datos -
+Strings -
+Numbers in Python -
+Obtener datos de usuario (input) -
+Word replacement exercise -
+Listas -
+Metodos de listas -
+Tuplas -
+Funciones -
+Return statements -
+If statements
+
+Python Exercise 2
+
+Diccionarios -
+While loops -
+
+
+"""
