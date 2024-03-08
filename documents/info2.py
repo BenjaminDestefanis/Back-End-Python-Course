24c9382f6a2df584ee96e083d0b5e17595ac3048
BenjaminDestefanis <benjaminisraelb@gmail.com>
2024-03-08 17:04:39 -0300

    3/8/24 5:04 PM

--- documents/info2.py
+++ documents/info2.py
@@ -1,4 +1,12 @@
 # Listas en python
+"""
+Caracteristicas:
+Secuencia mutable de elementos.
+Puede contener elementos de diferentes tipos de datos.
+Los elementos se pueden acceder mediante su índice.
+Los elementos se pueden modificar, agregar o eliminar.
+Se definen utilizando corchetes [].
+"""
 
 countris = ['United Kindon', 'EEUU', 'Ghana', 'Nigeria', 'Australia']
 user = 'pepe78'
@@ -16,5 +24,5 @@ print(countris)
 
 # Lists Methods
 
-# extend()  -  append() - count() - sort() - reverse() - .copy() - .pop([index que queremos borrar]) - .remove([]) - del - 
+# extend()  -  append() - count() - sort() - reverse() - .copy() - .pop([index que queremos borrar]) - .remove([]) - del - clear
 # EJ:  new_list = countris.copy()
--- documents/info3-tuplas.py
+++ documents/info3-tuplas.py
@@ -1,1 +1,21 @@
 # tuplas
+"""
+Caracteristicas:
+Secuencia inmutable de elementos.
+Puede contener elementos de diferentes tipos de datos.
+Los elementos se pueden acceder mediante su índice.
+Los elementos no se pueden modificar, agregar o eliminar una vez que la tupla está creada.
+Se definen utilizando paréntesis ().
+"""
+
+
+numbers = (1,2,3,4,2,2)
+strings = ('home','earth','land','earth')
+
+print(numbers, strings)
+print(type(numbers))
+
+# numbers[2] = 8 # Lanza  error
+
+print(numbers.count(2)) # Retorna el numero de veces que el elemnto esta en la tupla
+print(strings.index('earth')) # retorna el numero de indice si encuentra el elemtneto
